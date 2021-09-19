0de04e2e917be4f9cbdf8a34e5556c163b9a3fa5
dthtien <tien.dau@employmenthero.com>
2021-09-19 12:32:32 +0700

    ft(utils): add workflow name support

--- spec/dwf/utils_spec.rb
+++ spec/dwf/utils_spec.rb
@@ -20,4 +20,12 @@ describe Dwf::Utils, utils: true do
 
     it { expect(described_class.symbolize_keys(hash)).to eq expected }
   end
+
+  describe "#workflow_name?" do
+    FirstWorkflow = Class.new(Dwf::Workflow)
+    FirstJob = Class.new(Dwf::Item)
+
+    it { expect(described_class.workflow_name?(FirstWorkflow.name)).to be_truthy }
+    it { expect(described_class.workflow_name?(FirstJob.name)).to be_falsy }
+  end
 end
